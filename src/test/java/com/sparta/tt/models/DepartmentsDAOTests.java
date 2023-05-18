f80ef1bf797716fa846de9d811268a8a3b18cafd
Asad Ali <asadali.asif.1@gmail.com>
2023-05-18 20:02:28 +0100

    Remove unused imports

--- src/test/java/com/sparta/tt/models/DepartmentsDAOTests.java
+++ src/test/java/com/sparta/tt/models/DepartmentsDAOTests.java
@@ -1,16 +1,12 @@
 package com.sparta.tt.models;
 
-import com.sparta.tt.controllers.DepartmentsRepository;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class DepartmentsDAOTests {
     DepartmentsDAO mockDao = Mockito.mock(DepartmentsDAO.class);
