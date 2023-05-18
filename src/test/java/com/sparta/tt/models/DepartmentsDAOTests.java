d5727eb44f300843ef24f43b932d6012c1be3c92
Asad Ali <asadali.asif.1@gmail.com>
2023-05-18 20:00:57 +0100

    Add tests for DepartmentsDAO class

--- src/test/java/com/sparta/tt/models/DepartmentsDAOTests.java
+++ src/test/java/com/sparta/tt/models/DepartmentsDAOTests.java
@@ -1,2 +1,30 @@
-package com.sparta.tt.models;public class DepartmentsDAOTests {
+package com.sparta.tt.models;
+
+import com.sparta.tt.controllers.DepartmentsRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class DepartmentsDAOTests {
+    DepartmentsDAO mockDao = Mockito.mock(DepartmentsDAO.class);
+
+    @Test
+    @DisplayName("Check if getListOfDepartments returns an array of department names")
+    void checkGetListOfDepartments() throws SQLException {
+
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.when(resultSet.next()).thenReturn(true);
+
+        Mockito.when(mockDao.getAllDepartments()).thenReturn(resultSet);
+
+        Assertions.assertTrue(mockDao.getAllDepartments().next());
+
+    }
 }
