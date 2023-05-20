42aa4edd2ea639f7c3fff343b38d68f314e7adc1
Damien Gittings <damiengittings@hotmail.co.uk>
2023-05-20 13:02:06 +0100

    validate format of start and end date.  removed date validation to its own class. removed user date inputs from loop in loader

---
+++ src/main/java/com/sparta/tt/util/DateValidator.java
@@ -0,0 +1,16 @@
+package com.sparta.tt.util;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+public class DateValidator {
+    public static boolean dateValidator(String stDate, String enDate) {
+        try {
+            LocalDate startDate = LocalDate.parse(stDate);
+            LocalDate endDate = LocalDate.parse(enDate);
+            return endDate.isAfter(startDate);
+        } catch (DateTimeException e) {
+            return false;
+        }
+    }
+}
--- src/main/java/com/sparta/tt/views/InputValidator.java
+++ src/main/java/com/sparta/tt/views/InputValidator.java
@@ -11,12 +11,6 @@ public class InputValidator {
 
     private static ArrayList<String> departmentList = new ArrayList<>();
 
-    public static boolean dateValidator(String stDate, String enDate) {
-        LocalDate startDate = LocalDate.parse(stDate);
-        LocalDate endDate = LocalDate.parse(enDate);
-        return endDate.isAfter(startDate);
-    }
-
     public static boolean departmentValidator(int dept_id) {
         return dept_id > 0 && dept_id <= departmentList.size();
     }
